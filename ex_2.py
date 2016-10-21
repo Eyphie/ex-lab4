e2919bb7d73f6f37c5b1185c4cabd4e1181462c4
Eyphie <ira-1996@mail.ru>
2016-10-21 16:52:48 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -4,5 +4,39 @@ from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
+data3 = ['a', 'A', 'b', 'B']
 
 # Реализация задания 2
+first = Unique(data1)
+for i in first:
+    if i is None:
+        continue
+    else:
+        print(i, end=' ')
+print()
+
+
+second = Unique(list(data2))
+for i in second:
+    if i is None:
+        continue
+    else:
+        print(i, end=' ')
+print()
+
+
+third = Unique(data3)
+for i in third:
+    if i is None:
+        continue
+    else:
+        print(i, end=' ')
+print()
+
+fourth = Unique(data3, ignore_case=1)
+for i in fourth:
+    if i is None:
+        continue
+    else:
+        print(i, end=' ')
+print()
