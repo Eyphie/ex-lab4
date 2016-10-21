f99be6f15d1fdd99a0032026ecc463f5c48adcb2
Eyphie <ira-1996@mail.ru>
2016-10-21 16:53:52 +0300

    Update ex_6.py

--- ex_6.py
+++ ex_6.py
@@ -5,41 +5,32 @@ from librip.ctxmngrs import timer
 from librip.decorators import print_result
 from librip.gens import field, gen_random
 from librip.iterators import Unique as unique
+print(sys.getdefaultencoding())
 
-path = None
+path = sys.argv[1]
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
 
-with open(path) as f:
+with open(path, encoding="cp1251") as f:
     data = json.load(f)
-
-
-# Далее необходимо реализовать все функции по заданию, заменив `raise NotImplemented`
-# Важно!
-# Функции с 1 по 3 дожны быть реализованы в одну строку
-# В реализации функции 4 может быть до 3 строк
-# При этом строки должны быть не длиннее 80 символов
 
 @print_result
 def f1(arg):
-    raise NotImplemented
-
+    return sorted(unique([i for i in field(arg, 'job-name')], ignore_case=True), key=lambda x: x.lower())
 
 @print_result
 def f2(arg):
-    raise NotImplemented
-
+    return list(filter(lambda x: x.startswith('Программист'), arg))
 
 @print_result
 def f3(arg):
-    raise NotImplemented
-
+    return list(map(lambda x: x + " с опытом Python", arg))
 
 @print_result
 def f4(arg):
-    raise NotImplemented
-
+    sal = list(gen_random(100000, 200000, len(arg)))
+    return list('{}, зарплата {} руб.'.format(r, sal) for r, sal in zip(arg, sal))
 
 with timer():
     f4(f3(f2(f1(data))))
