5d75909e15426bbd3372e8f3bcce3f3b9c49e936
Eyphie <ira-1996@mail.ru>
2016-10-21 16:54:32 +0300

    Update decorators.py

--- librip/decorators.py
+++ librip/decorators.py
@@ -1,37 +1,16 @@
-# Здесь необходимо реализовать декоратор, print_result который принимает на вход функцию,
-# вызывает её, печатает в консоль имя функции, печатает результат и возвращает значение
-# Если функция вернула список (list), то значения должны выводиться в столбик
-# Если функция вернула словарь (dict), то ключи и значения должны выводить в столбик через знак равно
-# Пример из ex_4.py:
-# @print_result
-# def test_1():
-#     return 1
-#
-# @print_result
-# def test_2():
-#     return 'iu'
-#
-# @print_result
-# def test_3():
-#     return {'a': 1, 'b': 2}
-#
-# @print_result
-# def test_4():
-#     return [1, 2]
-#
-# test_1()
-# test_2()
-# test_3()
-# test_4()
-#
-# На консоль выведется:
-# test_1
-# 1
-# test_2
-# iu
-# test_3
-# a = 1
-# b = 2
-# test_4
-# 1
-# 2
+def print_result(func):
+    def decorated_func(*args):
+        print(func.__name__)
+        if len(args) > 0:
+            a = func(args[0])
+        else:
+            a = func()
+        if type(a) is list:
+            print("\n".join(map(str,a)))
+        elif type(a) is dict:
+            print('\n'.join([str(x)+"="+str(a[x]) for x in a]))
+        else:
+            print(a)
+        return(a)
+    return decorated_func
+
